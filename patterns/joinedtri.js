028e6cde8ed04fdfc6ff118acd61f774e716af6f
Adarsh <adarshtrip2306@gmail.com>
2025-01-14 23:46:21 +0530

    push

---
+++ patterns/joinedtri.js
@@ -0,0 +1,33 @@
+// let n = 9;
+
+// // Upper part of the diamond
+// for (let i = 0; i < n; i++) {
+// 	let row = "";
+// 	// Printing the spaces
+// 	for (let j = 0; j < n - i - 1; j++) {
+// 		row += " ";
+// 	}
+// 	// Printing the stars
+// 	for (let j = 0; j < i * 2 + 1; j++) {
+// 		row += "*";
+// 	}
+// 	console.log(row);
+// }
+
+// // Lower part of the diamond
+// for (let i = 1; i < n; i++) {
+// 	let row = "";
+// 	// Printing the spaces
+// 	for (let j = 0; j < i; j++) {
+// 		row += " ";
+// 	}
+// 	// Printing the stars
+// 	for (let j = 0; j < (n - i) * 2 - 1; j++) {
+// 		row += "*";
+// 	}
+// 	console.log(row);
+// }
+let N = 10;
+for (let i = 0; i <= N; i++) {
+	console.log(i);
+}
