8c0af300e5e3fa86b6528492dcb72f000cf9969c
Adarsh <adarshtrip2306@gmail.com>
2025-01-21 23:52:06 +0530

    ls using functional programming

--- Arrays/linearsearch.cpp
+++ Arrays/linearsearch.cpp
@@ -1,23 +1,27 @@
-//linear search
+// linear search
 #include <iostream>
 using namespace std;
-int main() {
-   int arr[5]={20,88,99,67,97};
-   int size = 5;
-   bool flag=0;
-   int target= 6;
-   for(int i =0;i<size;i++){
-        if(arr[i]==target){
-            flag=1;
+int main()
+{
+    int arr[5] = {20, 88, 99, 67, 97};
+    int size = 5;
+    bool flag = 0;
+    int target = 6;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+        {
+            flag = 1
         }
-
-   }
-   if(flag==1){
-        cout<< "elemet found";
-   }
-   else{
-        cout<< "element not found";
-   }
+    }
+    if (flag == 1)
+    {
+        cout << "elemet found";
+    }
+    else
+    {
+        cout << "element not found";
+    }
 
     return 0;
 }
