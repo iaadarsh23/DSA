1019d0ce1be6a22faaa70350b445441044009fa8
Adarsh <adarshtrip2306@gmail.com>
2025-01-21 22:37:14 +0530

    linearsearch by flag

---
+++ Arrays/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < arr.length; i++) {
+// 	if (arr[i] === key) {
+// 		console.log(`element found at ${i}`);
+// 	} else if (arr[i] !== key) {
+// 		continue;
+// 	} else {
+// 		console.log("element not found");
+// 	}
+// }
