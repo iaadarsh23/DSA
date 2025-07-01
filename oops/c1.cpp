e39ba3b8363aacf8d4a9ef97f27888f0f5dac865
Adarsh <adarshtrip2306@gmail.com>
2025-07-01 23:02:50 +0530

    linkedlistcreated

--- oops/c1.cpp
+++ oops/c1.cpp
@@ -4,26 +4,41 @@ using namespace std;
 //student class creation
 
 class Student{
-    int id;
-    string name;
-    int marks;
-    int noS;
-    //methods
+    private :
+        bool isGf;
 
-    void study(){
-        cout << "doing his fav task studying " << endl;
-    }
-    void bunk(){
-        cout << "doing his fav task bunking " << endl;
-    }
-    void gaming(){
-        cout << "doing his fav task gaming" << endl;
-    }
+
+   //BY DEFAULT ALL THE INFO UNDER A CLASS IS PRIVATE
+    public:
+        int id;
+        string name;
+        int marks;
+        int noS;
+        //methods
+
+        void study(){
+            cout << "doing his fav task studying " << endl;
+        }
+        void bunk(){
+            cout << "doing his fav task bunking " << endl;
+        }
+        void gaming(){
+            cout << "doing his fav task gaming" << endl;
+        }
+
+    private :
+        void gfChatting(){
+            cout << "chatting" << endl;
+        }    
 };
 int main()
 {
+    cout << sizeof(Student);
     Student s1;
+    s1.
+
+
+
     Student s2;
-    cout << sizeOf(Student);
     return 0;
 }
